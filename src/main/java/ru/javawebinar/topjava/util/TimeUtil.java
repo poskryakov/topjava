7047d617c0e565dc49ba45831b0aed720139dc43
Andrey Poskryakov <poskryakov@gmail.com>
2020-04-15 01:28:29 +0300

    Do HW0
    Implement filter of 'List<UserMeal>' using 'forEach' loops in method
    'UserMealsUtil.filteredByCycles'.
    
    Make minor changes for the sake of shorter code in other methods.
    
    - Add 'UserMeal.getDate', 'UserMeal.getTime'.
    - Add constructor 'UserMealWithExcess(UserMeal, boolean)'.
    - Add checks of 'TimeUtil.isBetweenHalfOpen' parameters.

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -1,9 +1,31 @@
 package ru.javawebinar.topjava.util;
 
 import java.time.LocalTime;
+import java.util.Objects;
 
 public class TimeUtil {
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
+
+    /**
+     * Checks if time is between two times.
+     * <p>
+     * The start time is included, but the end time is not.
+     *
+     * @param time  the time to check, not null
+     * @param startInclusive  start time, not null, strictly before {@code endExclusive}
+     * @param endExclusive  end time, not null, strictly after {@code startInclusive}
+     * @return true if time is between start time and end time
+     * @throws NullPointerException if any parameter is null
+     * @throws IllegalArgumentException if {@code startInclusive} is not strictly before {@code endExclusive}
+     */
+    public static boolean isBetweenHalfOpen(LocalTime time, LocalTime startInclusive, LocalTime endExclusive) {
+        Objects.requireNonNull(time, "Parameter time cannot be null.");
+        Objects.requireNonNull(startInclusive, "Parameter startInclusive cannot be null.");
+        Objects.requireNonNull(endExclusive, "Parameter endExclusive cannot be null.");
+
+        if (startInclusive.compareTo(endExclusive) >= 0) {
+            throw new IllegalArgumentException("Start time must be strictly before end time.");
+        }
+
+        return time.compareTo(startInclusive) >= 0 && time.compareTo(endExclusive) < 0;
     }
 }
--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -18,22 +18,35 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Ужин", 410),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак2", 500),
+                new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 29, 10, 0), "Обед", 2400)
         );
 
-        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        List<UserMealWithExcess> mealsTo;
+
+        System.out.println("filteredByCycles");
+        mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        mealsTo.sort(Comparator.comparing(UserMealWithExcess::toString));
         mealsTo.forEach(System.out::println);
-
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println();
     }
 
-    public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
-        return null;
-    }
+    public static List<UserMealWithExcess> filteredByCycles(
+            List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
 
-    public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO Implement by streams
-        return null;
+        Map<LocalDate, Integer> dailyCalories = new HashMap<>();
+        meals.forEach(meal -> dailyCalories.merge(meal.getDate(), meal.getCalories(), Integer::sum));
+
+        ArrayList<UserMealWithExcess> filteredMeals = new ArrayList<>(meals.size());
+        meals.forEach(meal -> {
+            if (TimeUtil.isBetweenHalfOpen(meal.getTime(), startTime, endTime)) {
+                boolean excess = dailyCalories.get(meal.getDate()) > caloriesPerDay;
+                filteredMeals.add(new UserMealWithExcess(meal, excess));
+            }
+        });
+        filteredMeals.trimToSize();
+
+        return filteredMeals;
     }
 }
